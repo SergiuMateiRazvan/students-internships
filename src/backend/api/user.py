e0bcf54660c22b8f42982f279c850d72f0f5dce7
Matei Sergiu <49958745+SergiuMateiRazvan@users.noreply.github.com>
2020-12-18 20:43:50 +0200

    Add login backend (#10)

---
+++ src/backend/api/user.py
@@ -0,0 +1,27 @@
+from crud import crud_user
+from db import session
+import fastapi
+from schema import schema_common, schema_user
+
+router = fastapi.APIRouter()
+
+
+@router.post("/user/", response_model=schema_common.Response)
+def create_user(user: schema_user.UserCreate):
+    with session.get_db_session() as db_session:
+        db_user = crud_user.get_user_by_mail(db_session, user.mail)
+        if db_user:
+            raise fastapi.HTTPException(
+                status_code=400, detail="Mail already bound to another user"
+            )
+        db_user = crud_user.create_user(db_session, user)
+        return schema_common.Response(message=f"User {db_user.mail} created")
+
+
+@router.post("/user/auth/", response_model=schema_user.UserGet)
+def get_user_by_mail(credentials: schema_user.UserAuth):
+    with session.get_db_session() as db_session:
+        db_user = crud_user.get_user_by_mail(db_session, credentials.mail)
+        if db_user and db_user.password == credentials.password:
+            return schema_user.UserGet.from_orm(db_user)
+        raise fastapi.HTTPException(status_code=404, detail="User not found")
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,4 @@
-from api import index, register, user_details
+from api import index, user, user_details
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -18,5 +18,5 @@ app.add_middleware(
 )
 
 app.include_router(index.router)
-app.include_router(register.router)
+app.include_router(user.router)
 app.include_router(user_details.router)
