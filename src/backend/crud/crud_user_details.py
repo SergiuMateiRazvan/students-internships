7a7bb7d32ee8e7246f69aa62c9fc0740e590995e
Matei Sergiu <49958745+SergiuMateiRazvan@users.noreply.github.com>
2020-12-18 10:22:42 +0200

    As user details get and update (#9)

--- src/backend/crud/crud_user_details.py
+++ src/backend/crud/crud_user_details.py
@@ -11,3 +11,13 @@ def create_user_details(db, details, user):
 
 def get_by_mail(db, mail):
     return db.query(base.UserDetails).filter(base.UserDetails.user_mail == mail).first()
+
+
+def update_user_details(db, mail, update_data):
+    if (
+        not db.query(base.UserDetails)
+        .filter(base.UserDetails.user_mail == mail)
+        .update(update_data)
+    ):
+        return None
+    return get_by_mail(db, mail)
