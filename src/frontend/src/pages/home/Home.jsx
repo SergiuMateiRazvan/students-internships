b7847bce7f8880dcba464a7cc3a5474c00d69d26
Sergiu Matei <sergiu.matei@ly.st>
2021-01-09 20:46:52 +0200

    Add sorting

--- src/frontend/src/pages/home/Home.jsx
+++ src/frontend/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react';
 import {InternshipList} from './InternshipList';
 import {getInternships} from '../../service';
 import {SearchBar} from './SearchBar';
+import {Sort} from './Sort';
 import {HeaderNav} from '../common/HeaderNav';
 
 export const Home = () => {
@@ -16,14 +17,20 @@ export const Home = () => {
   }, []);
 
   const onSearch = (searchText) => {
-    getInternships({title: searchText}).then((response) => {
-      setInternshipList(response);
-    });
+    getInternships({title: searchText}).then((response) =>
+      setInternshipList(response),
+    );
+  };
+
+  const onSort = (sortField, sortDirection='ascending') => {
+    getInternships({}, {sort: sortField, sort_direction: sortDirection})
+        .then((response) => setInternshipList(response));
   };
 
   return (
     <div className={'outerWrapper'}>
       <HeaderNav />
+      <Sort onSort={onSort} />
       <SearchBar onSearch={onSearch} />
       <InternshipList internships={internshipsList}/>
     </div>
